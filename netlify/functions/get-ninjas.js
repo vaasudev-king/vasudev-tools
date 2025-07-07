bc3ce2b4f9f638466b168562d58aad361af544d1
Vaasudev Uzumaki <141260443+vaasudev-king@users.noreply.github.com>
2025-07-07 14:20:04 +0530

    Update get-ninjas.js

--- netlify/functions/get-ninjas.js
+++ netlify/functions/get-ninjas.js
@@ -1,22 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, "ninjas.json");
+exports.handler = async () => {
+  const filePath = path.join(__dirname, "../../ninjas.json");
+  const data = fs.existsSync(filePath) ? JSON.parse(fs.readFileSync(filePath)) : [];
 
-exports.handler = async function () {
-  try {
-    const data = fs.existsSync(filePath)
-      ? JSON.parse(fs.readFileSync(filePath, "utf-8"))
-      : [];
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Failed to read ninja list." }),
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
 };
